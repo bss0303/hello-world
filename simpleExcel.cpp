68c1a8987222a1aa4ff7bc47462d015b95d015f5
bss0303 <40429262+bss0303@users.noreply.github.com>
2019-03-13 21:04:24 +0800

    the one before finish

--- simpleExcel.cpp
+++ simpleExcel.cpp
@@ -7,6 +7,7 @@
 #include<cctype>
 #include<cstdlib>
 #include<cstring>
+#include<stack>
 using namespace std;
 
 const int maxn=1003, maxm=29;
@@ -48,23 +49,24 @@ int main(){
 		while(getline(strin, table[i][x++].content,','));
 	}
 //test input
-/*	for(int i = 1; i<=n; i++){
+cout <<"test input---->" <<endl;
+	for(int i = 1; i<=n; i++){
 		for(int j = 1;j<=m; j++){
 			cout <<table[i][j].content <<" ";
 		}
 		cout <<endl;
 	}
-*/
+
 	//在判断类型的同时，建图
 	for(int i=1; i<=n; i++){
 		for(int j=1; j<=m; j++){
 			Cell &x = table[i][j];
-			if(x.content == "") x.content="0"; 
-			else if(x.content[0] == '='){
+			if(x.content == "") x.content="0";//1.处理空白符 
+			else if(x.content[0] == '='){//2.处理函数
 				x.isValue = true;
 				strToUpper(x.content);//都变成大写
-				addEdges((i-1)*m+j,x.content);//根据公式向图中加入边
-			}else if(check(x.content)){
+				addEdges((i-1)*m+j, x.content);//根据公式向图中加入边
+			}else if(check(x.content)){//3.处理数值
 				x.isValue=1;
 				x.value=tod(x.content);
 			}
@@ -79,6 +81,7 @@ int main(){
 		}
 	}
 //test graph
+cout <<"test graph----->" <<endl;
 	for(int i=1; i<=n*m; i++){
 		cout <<i<<":";
 		for(int j=head[i]; edges[j].from ==i && j<=n*m; j++){
@@ -86,13 +89,24 @@ int main(){
 		}
 		cout <<endl;
 	}
-
+//test top
+/*
+cout <<"test Top()----->" <<endl;
+	if(Top()) cout << "YES" <<endl;
+	else cout <<"ERROR!" << endl;
+*/
+	if(!Top()){
+		cout <<"error!" <<endl;
+		return 0;
+	}
 	//output
+cout <<"test output----->" <<endl;
 	for(int i = 1; i<=n; i++){
 		for(int j = 1;j<=m; j++){
 			Cell &x = table[i][j];//make a alias
-			if(x.isValue) cout <<x.value <<",";
-			else cout <<x.content <<",";
+			if(x.isValue) printf("%.2lf", x.value);
+			else cout <<x.content;
+			if(j<m) printf(",");
 		}
 		cout <<endl;
 	}
@@ -119,40 +133,58 @@ void strToUpper(string& str){//所有小写变大写
 
 void addEdges(int f, string str){
 	for(int i=0; i<str.length()-1; i++){
-		int x1=0, y1=0, x2=0, y2=0;
+		int x1=0,y1=0,x2=0,y2=0;
 		if(isalpha(str[i]) && isdigit(str[i+1])){
-			int y1 = str[i]-64, x1=0;
+			y1 = str[i]-64, x1=0;
 			while(i<str.length()-1 && isdigit(str[i+1])){
 				x1=x1*10+(str[++i]-'0');
 			}
-		}
-		if(str[i+1] == ':'){
-			i+=2;//a1:b1
-			int y2 = str[i]-64, x2=0;
-			while(i<str.length()-1 && isdigit(str[i+1])){
-				x2=x2*10+(str[++i]-'0');
-			}
-			if(x1>x2) swap(x1,x2);
-			if(y1>y2) swap(y1,y2);
-			for(int j=x1; j<=x2; j++){
-				for(int k=y1;k<=y2;k++){
-					Edge &x = edges[cnt];
-					x.from=(j-1)*m+k;
-					x.to = f;
-					cnt++;
-					inDegree[f]++;
+
+			if(i<str.length()-1 &&  str[i+1] == ':'){
+				i+=2;//a1:b1
+				y2 = str[i]-64, x2=0;
+				while(i<str.length()-1 && isdigit(str[i+1])){
+					x2=x2*10+(str[++i]-'0');
 				}
+			cout <<x1 <<" "<<y1 <<" " <<x2 <<" " <<y2 <<endl;
+				if(x1>x2) swap(x1,x2);
+				if(y1>y2) swap(y1,y2);
+				for(int j=x1; j<=x2; j++){
+					for(int k=y1;k<=y2;k++){
+						Edge &x = edges[cnt];
+						x.from=(j-1)*m+k;
+						x.to = f;
+						cnt++;
+						inDegree[f]++;
+					}
+				}
+			}else{
+				Edge &x = edges[cnt];
+				x.from=(x1-1)*m+y1;
+				x.to = f;
+				cnt++;
+				inDegree[f]++;
 			}
-			continue;
 		}
-		Edge &x = edges[cnt];
-		x.from=(x1-1)*m+y1;
-		x.to = f;
-		cnt++;
-		inDegree[f]++;
 	}
 }
 bool Top(){
+	stack<int> sk;
+	for(int i=1; i<=n*m; i++)
+		if(inDegree[i]==0)
+			sk.push(i);
+
+	int k=n*m;
+	while(!sk.empty()){
+		int p = sk.top(); sk.pop(); k--;
+		if(head[p]==-1) continue;//出度为0,跳过
+
+		for(int i=head[p]; edges[i].from == p && i<cnt; i++){
+			int x = edges[i].to;
+			if((--inDegree[x])==0) sk.push(x);
+		}
+	}
+	return k==0?true:false;
 }
 
 double Cell::evaluate(){return 1;}
